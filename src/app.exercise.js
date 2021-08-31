1b3717d80a79027519c3449e03dc55def1033d29
Coen88 <coen_coenowy@o2.pl>
2021-08-31 16:42:51 +0200

    Auth exercises

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -2,31 +2,47 @@
 import {jsx} from '@emotion/core'
 
 import * as React from 'react'
-// 🐨 you're going to need this:
 import * as auth from 'auth-provider'
+import {client} from './utils/api-client.exercise'
 import {AuthenticatedApp} from './authenticated-app'
 import {UnauthenticatedApp} from './unauthenticated-app'
 
+const getUser = async () => {
+  let user = null
+  const token = await auth.getToken()
+  
+  if (token) {
+    user = await client('me', {token})
+  }
+}
+
 function App() {
-  // 🐨 useState for the user
   const [user, setUser] = React.useState(null)
 
-  // 🐨 create a login function that calls auth.login then sets the user
+  
+  // React.useEffect(() => {
+  //   const fetchData = async () => {
+
+  //     const token = await auth.getToken()
+      
+  //     if (token) {
+  //       // we're logged in! Let's go get the user's data:
+  //       auth.client('me', {token}).then(data => {
+  //         console.log(data.user)
+  //       })
+  //     } else {
+  //       // we're not logged in. Show the login screen
+  //     }
+  //   }
+  // })
+    
   const login = form => auth.login(form).then(u => setUser(u))
-  // 💰 const login = form => auth.login(form).then(u => setUser(u))
-  // 🐨 create a registration function that does the same as login except for register
   const register = form => auth.register(form).then(u => setUser(u))
 
-  // 🐨 create a logout function that calls auth.logout() and sets the user to null
   const logout = () => {
     auth.logout()
     setUser(null)
   }
-
-  // 🐨 if there's a user, then render the AuthenticatedApp with the user and logout
-
-
-  // 🐨 if there's not a user, then render the UnauthenticatedApp with login and register
 
   return user ? <AuthenticatedApp user={user} logout={logout} /> : <UnauthenticatedApp login={login} register={register} />
 }
